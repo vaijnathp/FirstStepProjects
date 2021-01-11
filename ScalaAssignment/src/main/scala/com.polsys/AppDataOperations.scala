33445351a7015f8b0109aafeff54965eab09c01a
Vaijnath Kailas Polsane <vaijnath.polsane@yash.com>
2021-01-12 04:36:12 +0530

    Added missing condition

--- ScalaAssignment/src/main/scala/com.polsys/AppDataOperations.scala
+++ ScalaAssignment/src/main/scala/com.polsys/AppDataOperations.scala
@@ -8,12 +8,12 @@ object AppDataOperations {
   def main(args: Array[String]): Unit = {
     val session = SparkSession.builder().master("local[*]").appName("LongestWord").getOrCreate()
 
-      val schema = new StructType().add("app_id", StringType)
-        .add("app_store", StringType)
-        .add("app_url", StringType)
-        .add("version", StringType)
-        .add("supported_language",ArrayType(StringType))
-        .add("category", StringType)
+    val schema = new StructType().add("app_id", StringType)
+      .add("app_store", StringType)
+      .add("app_url", StringType)
+      .add("version", StringType)
+      .add("supported_language",ArrayType(StringType))
+      .add("category", StringType)
     val appData = session.read.schema(schema).option("inferSchema",true).json(args(0)).cache()
     val appAvailability = appData.filter(col("app_store").isin(Seq("itunes","playstore"):_*) || col("app_url").isNotNull)
     println("************valid app based on availability on itunes or playstore or valid app store URL*********")
@@ -23,7 +23,7 @@ object AppDataOperations {
     println("************valid app based only from itunes*********")
     appFromItunes.show(truncate = false)
 
-    val gamingApp = appData.filter(col("category")==="Gaming")
+    val gamingApp = appData.filter(col("app_store")==="itunes" and col("category")==="Gaming")
     println("************valid app based only from itunes which are of Gaming category*********")
     gamingApp.show(truncate = false)
 
